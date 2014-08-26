568d546fa14f0ad33a0bb46e2c66ddfdf4365e0e
Antoine Cronier <antoinecronier@live.fr>
2014-08-26 17:23:49 +0200

    Add true Load balancer URL

---
+++ functions.php
@@ -0,0 +1,3 @@
+﻿<?php
+update_option('siteurl','http://myloadbalancer-2133764557.us-west-2.elb.amazonaws.com');
+update_option('home','http://myloadbalancer-2133764557.us-west-2.elb.amazonaws.com');
